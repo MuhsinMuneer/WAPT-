feba7454b12bb52ed97a185779f419a25e405e51
MUHSIN BIN MUHAMMED MUNEER <49632623+MuhsinMuneer@users.noreply.github.com>
2025-04-02 21:27:40 +0530

    Update app.py
    color change graph , criticality

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ if uploaded_file:
 
         with col2:
             fig2 = px.bar(high_vuln_apps_by_BA, x=high_vuln_apps_by_BA.index, y=high_vuln_apps_by_BA.values,
-                          labels={"y": "High Severity Apps", "x": "Business Area"}, title="High Vulnerability Apps by Business Area", color_discrete_sequence=["red"],text_auto=True)
+                          labels={"y": "High Severity Apps", "x": "Business Area"}, title="High Vulnerability Apps by Business Area", color_discrete_sequence=["red", "orange", "yellow"],text_auto=True)
             st.plotly_chart(fig2, use_container_width=True)
 
         st.markdown("### Findings Status Distribution")
@@ -156,7 +156,7 @@ if uploaded_file:
 
         with col4:
             fig4 = px.bar(open_findings, barmode="stack", labels={"value": "Open Findings Count", "Business Area": "Business Area"},
-                          title="Open Findings by Business Area", color_discrete_sequence=["orange"],text_auto=True)
+                          title="Open Findings by Business Area", color_discrete_sequence=["red", "orange", "yellow"],text_auto=True)
             st.plotly_chart(fig4, use_container_width=True)
 
         st.markdown("### Overdue & Severity Distribution")
